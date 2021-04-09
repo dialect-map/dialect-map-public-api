46f1498566c471f28bd3d527f196cccd5dee8412
Sinclert Pérez <Sinclert@hotmail.com>
2021-04-09 10:37:13 -0400

    Simplify main.py setup functions

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask_cors import CORS
+
 from config import ApplicationConfig
 from config import EnvironmentConfigLoader
 from globals import setup_service
@@ -12,41 +13,31 @@ app = Flask(__name__)
 cors = CORS(app, methods=["GET"])
 
 
-def setup_encoding():
-    """ Setup the Flask encoding / decoding classes """
+def create_app():
+    """ Initializes the Flask application entity """
 
     from dialect_map.encoding import CustomJSONDecoder
     from dialect_map.encoding import CustomJSONEncoder
 
-    app.json_decoder = CustomJSONDecoder
-    app.json_encoder = CustomJSONEncoder
-
-
-def setup_routes():
-    """ Setup all the Flask blueprint routes """
-
+    from handlers import clean_session
+    from handlers import error_mappings
     from routes import all_blueprints
 
+    # Setup all the blueprint routes
     for bp in all_blueprints:
         app.register_blueprint(bp)
 
-
-def setup_errors_handlers():
-    """ Setup all the Flask exception handlers """
-
-    from handlers import error_mappings
-
+    # Setup all the error handlers
     for mapping in error_mappings:
         for error in mapping["errors"]:
             app.register_error_handler(error, mapping["handler"])
 
-
-def setup_context_handlers():
-    """ Setup all the Flask context handlers """
-
-    from handlers import clean_session
-
+    # Setup all the context handlers
     app.teardown_request(clean_session)
+
+    # Setup the encoding / decoding classes
+    app.json_decoder = CustomJSONDecoder
+    app.json_encoder = CustomJSONEncoder
 
 
 # Gunicorn running the server
@@ -55,15 +46,10 @@ if __name__ == "main":
     loader = EnvironmentConfigLoader()
     config = ApplicationConfig(loader)
 
-    # Setup the logger first
     setup_logger(config.log_level)
     setup_service(c=config)
 
-    setup_encoding()
-
-    setup_routes()
-    setup_errors_handlers()
-    setup_context_handlers()
+    create_app()
 
 
 # Flask running the server
@@ -72,12 +58,7 @@ if __name__ == "__main__":
     loader = EnvironmentConfigLoader()
     config = ApplicationConfig(loader)
 
-    # Setup the logger first
     setup_logger(config.log_level)
     setup_service(c=config)
 
-    setup_encoding()
-
-    setup_routes()
-    setup_errors_handlers()
-    setup_context_handlers()
+    create_app()
