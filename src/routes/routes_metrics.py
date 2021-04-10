2bfebe1033fafc7cc18489eb706c9fb92ed358b0
Sinclert Pérez <Sinclert@hotmail.com>
2021-04-09 21:00:05 -0400

    Support papers old ID format (II)

--- src/routes/routes_metrics.py
+++ src/routes/routes_metrics.py
@@ -4,6 +4,8 @@ from flask import Blueprint
 from flask import jsonify
 from globals import service
 
+from .__utils import build_paper_id
+
 
 bp = Blueprint("metrics", __name__)
 
@@ -37,7 +39,7 @@ def get_cat_metrics_by_jargon(jargon_id: str, category_id: str = None):
     return jsonify(records_data), 200
 
 
-# ----------- Paper Jargon Metrics model ----------- #
+# ------------ Paper Jargon Metrics model ------------ #
 
 
 @bp.route("/paper/metrics/<metric_id>", methods=["GET"])
@@ -53,14 +55,24 @@ def get_paper_metrics(metric_id: str):
 
 
 @bp.route("/paper/<paper_id>/<paper_rev>/metrics/jargon/<jargon_id>", methods=["GET"])
-def get_paper_metrics_by_jargon(jargon_id: str, paper_id: str = None, paper_rev: int = None):
+@bp.route("/paper/<category>/<paper_id>/<paper_rev>/metrics/jargon/<jargon_id>", methods=["GET"])
+def get_paper_metrics_by_jargon(
+    jargon_id: str,
+    category: str = None,
+    paper_id: str = None,
+    paper_rev: int = None,
+):
     """
     Gets a paper jargon metric from the underlying database
     :param jargon_id: ID of the jargon to get metrics from
+    :param category: name of the paper main category (optional)
     :param paper_id: ID of the paper to filter metrics by (optional)
     :param paper_rev: revision of the paper to filter metrics by (optional)
     :return: HTTP 200 response
     """
+
+    if paper_id:
+        paper_id = build_paper_id(paper_id, category)
 
     records = service.jargon_paper_metrics.get_by_jargon(jargon_id, paper_id, paper_rev)
     records_data = [record.data for record in records]
