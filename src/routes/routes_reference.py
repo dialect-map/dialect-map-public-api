7a75bd2647b5577f35ff00ad85cee4df3b0e79bf
Sinclert Pérez <Sinclert@hotmail.com>
2021-04-11 16:01:49 -0400

    Remove paper ID old format attempt (III)

--- src/routes/routes_reference.py
+++ src/routes/routes_reference.py
@@ -4,8 +4,6 @@ from flask import Blueprint
 from flask import jsonify
 from globals import service
 
-from .__utils import build_paper_id
-
 
 bp = Blueprint("references", __name__)
 
@@ -23,18 +21,13 @@ def get_reference(reference_id: str):
 
 
 @bp.route("/references/source/<paper_id>/<paper_rev>", methods=["GET"])
-@bp.route("/references/source/<category>/<paper_id>/<paper_rev>", methods=["GET"])
-def get_references_by_source_paper(paper_id: str, paper_rev: int, category: str = None):
+def get_references_by_source_paper(paper_id: str, paper_rev: int):
     """
     Gets a paper reference from the underlying database
     :param paper_id: ID of the source paper references
     :param paper_rev: revision of the source paper references
-    :param category: name of the paper main category (optional)
     :return: HTTP 200 response
     """
-
-    # Builds a valid paper ID
-    paper_id = build_paper_id(paper_id, category)
 
     records = service.paper_refs.get_by_source_paper(paper_id, paper_rev)
     records_data = [record.data for record in records]
@@ -42,18 +35,13 @@ def get_references_by_source_paper(paper_id: str, paper_rev: int, category: str 
 
 
 @bp.route("/references/target/<paper_id>/<paper_rev>", methods=["GET"])
-@bp.route("/references/target/<category>/<paper_id>/<paper_rev>", methods=["GET"])
-def get_references_by_target_paper(paper_id: str, paper_rev: int, category: str = None):
+def get_references_by_target_paper(paper_id: str, paper_rev: int):
     """
     Gets a paper reference from the underlying database
     :param paper_id: ID of the target paper references
     :param paper_rev: revision of the target paper references
-    :param category: name of the paper main category (optional)
     :return: HTTP 200 response
     """
-
-    # Builds a valid paper ID
-    paper_id = build_paper_id(paper_id, category)
 
     records = service.paper_refs.get_by_target_paper(paper_id, paper_rev)
     records_data = [record.data for record in records]
