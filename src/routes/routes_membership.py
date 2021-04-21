5428ec8d595fe6f3075bfa8e7393a3b3484fed99
Sinclert Pérez <Sinclert@hotmail.com>
2021-04-21 18:11:48 -0400

    Define member. get_by_paper route

--- src/routes/routes_membership.py
+++ src/routes/routes_membership.py
@@ -18,3 +18,17 @@ def get_membership(membership_id: str):
 
     record = service.category_memberships.get(membership_id)
     return jsonify(record.data), 200
+
+
+@bp.route("/category/membership/paper/<path:paper_id>/rev/<paper_rev>", methods=["GET"])
+def get_membership_by_paper(paper_id: str, paper_rev: int):
+    """
+    Gets a list of memberships from the underlying database
+    :param paper_id: ID of the paper to filter memberships by
+    :param paper_rev: revision of the paper to filter memberships by
+    :return: HTTP 200 response
+    """
+
+    records = service.category_memberships.get_by_paper(paper_id, paper_rev)
+    records_data = [record.data for record in records]
+    return jsonify(records_data), 200
