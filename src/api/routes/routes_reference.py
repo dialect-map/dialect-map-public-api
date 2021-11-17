aad1edf2df5a8b087396e78110a527f2e6e0e7cb
Sinclert Pérez <Sinclert@hotmail.com>
2021-11-17 14:00:22 -0500

    Write OpenAPI spec for ref. routes

--- src/api/routes/routes_reference.py
+++ src/api/routes/routes_reference.py
@@ -14,9 +14,23 @@ bp = Blueprint("references", __name__)
 @bp.get("/reference/<reference_id>")
 def get_reference(reference_id: str):
     """
-    Gets a paper reference from the underlying database
-    :param reference_id: ID of the paper reference to get
-    :return: HTTP 200 response
+    ArXiv reference endpoint
+    ---
+    get:
+      description: Get an ArXiv reference from the database
+      parameters:
+        - name: reference_id
+          in: path
+          description: ArXiv reference identifier
+          required: true
+          schema:
+            type: string
+      responses:
+        200:
+          description: ArXiv reference JSON record
+          content:
+            application/json:
+              schema: PaperReferenceSchema
     """
 
     ref = service.paper_refs.get(reference_id)
@@ -29,10 +43,31 @@ def get_reference(reference_id: str):
 @bp.get("/references/source/<path:paper_id>/rev/<paper_rev>")
 def get_references_by_source_paper(paper_id: str, paper_rev: int):
     """
-    Gets a paper reference from the underlying database
-    :param paper_id: ID of the source paper references
-    :param paper_rev: revision of the source paper references
-    :return: HTTP 200 response
+    ArXiv reference by source paper endpoint
+    ---
+    get:
+      description: Get a list of ArXiv references from the database
+      parameters:
+        - name: paper_id
+          in: path
+          description: ArXiv paper identifier
+          required: true
+          schema:
+            type: string
+        - name: paper_rev
+          in: path
+          description: ArXiv paper revision
+          required: true
+          schema:
+            type: integer
+      responses:
+        200:
+          description: ArXiv reference JSON records
+          content:
+            application/json:
+              schema:
+                type: array
+                items: PaperReferenceSchema
     """
 
     refs = service.paper_refs.get_by_source_paper(paper_id, paper_rev)
@@ -45,10 +80,31 @@ def get_references_by_source_paper(paper_id: str, paper_rev: int):
 @bp.get("/references/target/<path:paper_id>/rev/<paper_rev>")
 def get_references_by_target_paper(paper_id: str, paper_rev: int):
     """
-    Gets a paper reference from the underlying database
-    :param paper_id: ID of the target paper references
-    :param paper_rev: revision of the target paper references
-    :return: HTTP 200 response
+    ArXiv reference by target paper endpoint
+    ---
+    get:
+      description: Get a list of ArXiv references from the database
+      parameters:
+        - name: paper_id
+          in: path
+          description: ArXiv paper identifier
+          required: true
+          schema:
+            type: string
+        - name: paper_rev
+          in: path
+          description: ArXiv paper revision
+          required: true
+          schema:
+            type: integer
+      responses:
+        200:
+          description: ArXiv reference JSON records
+          content:
+            application/json:
+              schema:
+                type: array
+                items: PaperReferenceSchema
     """
 
     refs = service.paper_refs.get_by_target_paper(paper_id, paper_rev)
